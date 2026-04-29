420c4a23c0a9a7027a3bdf4ac8558b0fdd2f794c
hesch <henri@reschmi.de>
2026-04-29 15:25:34 +0900

    add some neovim config

--- dot_config/nvim/lua/hesch/lazy/fugitive.lua
+++ dot_config/nvim/lua/hesch/lazy/fugitive.lua
@@ -1,9 +1,16 @@
 return {
-    "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gst", vim.cmd.Git)
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gst", vim.cmd.Git)
 
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
-    end
+            vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+            vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+        end
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+        end
+    }
 }
