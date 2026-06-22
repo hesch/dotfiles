4c644290ac8b4e9f078f44e5ea6a62fd6fc72f97
henri.schmidt <henri.schmidt@zdf.jp>
2026-06-22 15:03:09 +0900

    add hidden switch option to telescope

--- dot_config/nvim/lua/hesch/lazy/telescope.lua
+++ dot_config/nvim/lua/hesch/lazy/telescope.lua
@@ -27,13 +27,39 @@ return {
                     --   codeactions = false,
                     -- }
                 }
-            }
+            },
         })
         require("telescope").load_extension("ui-select")
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = false }) end, {})
-        vim.keymap.set('n', '<leader>pF', function() builtin.find_files({ hidden = true }) end, {})
+
+        local my_find_files
+        my_find_files = function(opts, no_ignore)
+            opts = opts or {}
+            no_ignore = vim.F.if_nil(no_ignore, false)
+            opts.attach_mappings = function(_, map)
+                map({ "n", "i" }, "<C-h>", function(prompt_bufnr) -- <C-h> to toggle modes
+                    local prompt = require("telescope.actions.state").get_current_line()
+                    require("telescope.actions").close(prompt_bufnr)
+                    no_ignore = not no_ignore
+                    my_find_files({ default_text = prompt }, no_ignore)
+                end)
+                return true
+            end
+
+            if no_ignore then
+                opts.no_ignore = true
+                opts.hidden = true
+                opts.prompt_title = "Find Files <ALL>"
+                builtin.find_files(opts)
+            else
+                opts.prompt_title = "Find Files"
+                builtin.find_files(opts)
+            end
+        end
+
+        vim.keymap.set('n', '<leader>pf', function() my_find_files({}, false) end, {})
+        vim.keymap.set('n', '<leader>pF', function() my_find_files({}, true) end, {})
         vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pr', builtin.resume, {})
         vim.keymap.set('n', '<leader>pws', function()
